bbd56ff558051e72ecfe966c202f75cde059f658
Amaury Vasquez <amaury.vasquez@prescriberpoint.com>
2025-11-24 14:24:18 -0600

    feat: remove transition and remove action for theme

--- app/actions/theme.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { cookies } from "next/headers";
-import { THEME_COOKIE } from "@/constants/cookies";
-
-export async function getThemeCookie() {
-  const cookieStore = await cookies();
-  return cookieStore.get(THEME_COOKIE)?.value;
-}
--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,31 +1,29 @@
-import { useCallback, useEffect, useTransition } from "react";
+import { useCallback, useEffect } from "react";
 import { useCookies } from "react-cookie";
-import { getThemeCookie } from "@/app/actions/theme";
 import { THEME_COOKIE } from "@/constants/cookies";
 
+const getDocumentHTML = () => document.querySelector("html");
+
 export const useTheme = () => {
-  const [_isPending, startTransition] = useTransition();
   const [cookies, setCookie] = useCookies([THEME_COOKIE]);
 
   const handleThemeChange = useCallback(
     (theme?: string) => {
       if (theme) {
-        document.querySelector("html")?.setAttribute("data-theme", theme);
+        getDocumentHTML()?.setAttribute("data-theme", theme);
         setCookie(THEME_COOKIE, theme);
       }
     },
     [setCookie]
   );
 
-  useEffect(() => {
-    startTransition(async () => {
-      const themeCookie = await getThemeCookie();
-      setCookie(THEME_COOKIE, themeCookie);
-      handleThemeChange(themeCookie);
-    });
-  }, [handleThemeChange, setCookie]);
-
   const theme = cookies[THEME_COOKIE];
+
+  useEffect(() => {
+    if (theme && getDocumentHTML()?.getAttribute("data-theme") !== theme) {
+      handleThemeChange(theme);
+    }
+  }, [theme, handleThemeChange]);
 
   return {
     theme,
